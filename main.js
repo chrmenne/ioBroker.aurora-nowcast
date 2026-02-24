4ed2f33d44bc2316b5a3329df50be74487486e81
Christian Menne <publicdevelopment@christianmenne.de>
2026-02-24 16:59:41 +0100

    improved validation

--- main.js
+++ main.js
@@ -22,8 +22,6 @@ class AuroraBorealis extends utils.Adapter {
 		});
 		this.on("ready", this.onReady.bind(this));
 		this.on("stateChange", this.onStateChange.bind(this));
-		// this.on("objectChange", this.onObjectChange.bind(this));
-		// this.on("message", this.onMessage.bind(this));
 		this.on("unload", this.onUnload.bind(this));
 	}
 
@@ -45,7 +43,7 @@ class AuroraBorealis extends utils.Adapter {
 		const timeout = setTimeout(() => controller.abort(), 10000);
 		let json;
 		try {
-			const res = await fetch("https://services.swpc.noaa.gov/json/ovation_aurora_latest.json", {
+			const res = await fetch(this.config.ovationUrl, {
 				signal: controller.signal,
 				headers: {
 					"User-Agent": "ioBroker-aurora-borealis",
@@ -155,12 +153,6 @@ class AuroraBorealis extends utils.Adapter {
 	 */
 	onUnload(callback) {
 		try {
-			// Here you must clear all timeouts or intervals that may still be active
-			// clearTimeout(timeout1);
-			// clearTimeout(timeout2);
-			// ...
-			// clearInterval(interval1);
-
 			callback();
 		} catch (error) {
 			this.log.error(`Error during unloading: ${error.message}`);
@@ -168,22 +160,6 @@ class AuroraBorealis extends utils.Adapter {
 		}
 	}
 
-	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-	// /**
-	//  * Is called if a subscribed object changes
-	//  * @param {string} id
-	//  * @param {ioBroker.Object | null | undefined} obj
-	//  */
-	// onObjectChange(id, obj) {
-	// 	if (obj) {
-	// 		// The object was changed
-	// 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-	// 	} else {
-	// 		// The object was deleted
-	// 		this.log.info(`object ${id} deleted`);
-	// 	}
-	// }
 
 	/**
 	 * Is called if a subscribed state changes
@@ -208,23 +184,6 @@ class AuroraBorealis extends utils.Adapter {
 			this.log.info(`state ${id} deleted`);
 		}
 	}
-	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-	// /**
-	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-	//  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-	//  * @param {ioBroker.Message} obj
-	//  */
-	// onMessage(obj) {
-	// 	if (typeof obj === "object" && obj.message) {
-	// 		if (obj.command === "send") {
-	// 			// e.g. send email or pushover or whatever
-	// 			this.log.info("send command");
-
-	// 			// Send response in callback if required
-	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-	// 		}
-	// 	}
-	// }
 }
 
 if (require.main !== module) {
